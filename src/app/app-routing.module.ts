56913a8b2fe86a616b9928d5a2f31450263ed9f0
brooke ryan <brooke.ryan@modaoperandi.com>
2018-11-28 19:17:46 -0500

    PIM first view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { PacketDetailComponent } from './views/packet-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'packet', pathMatch: 'full' },
+  { path: 'packet', component: PacketDetailComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
---
+++ src/app/views/packet-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'packet-detail.component.html',
+  styleUrls: ['packet-detail.component.scss']
+})
+
+export class PacketDetailComponent {}
+
+
