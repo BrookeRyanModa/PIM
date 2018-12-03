f674fce51987a39de8f9cb52658f5b7f096e28af
brooke ryan <brooke.ryan@modaoperandi.com>
2018-12-03 17:05:34 -0500

    PIM added stepper

--- src/app/views/packet-detail.component.ts
+++ src/app/views/packet-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Packet } from './packet';
 import { Item } from './item';
-import { ItemDetailComponent } from './item-detail.component';
+// import { ItemDetailComponent } from './item-detail.component';
 
 @Component({
   templateUrl: 'packet-detail.component.html',
