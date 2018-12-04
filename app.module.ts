f96efe006e75438b258596ba72919d12741a8f9e
BrookeRyanModa <43546694+BrookeRyanModa@users.noreply.github.com>
2018-12-03 21:30:09 -0500

    [master]
    added edit button, ready for link to form

---
+++ app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { PacketDetailComponent } from './views/packet-detail.component';
+import { ItemDetailComponent } from './views/item-detail.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PacketDetailComponent,
+    ItemDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    MatStepperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
